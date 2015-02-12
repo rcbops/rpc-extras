14a2b2a7c0aacad0d4f3b90f092b33db0b669b5f
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-02-11 18:54:27 -0800

    ansible galaxy restructure

--- inventory/dynamic_inventory.py
+++ inventory/dynamic_inventory.py
@@ -5,9 +5,9 @@ import subprocess
 import yaml
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-vars_yaml = os.path.join(basedir, '../vars/common.yml')
+vars_yaml = os.path.join(basedir, '../global_vars/all/config.yml')
 rpc_repo_path = yaml.load(open(vars_yaml, 'r'))['rpc_repo_path']
 
 rpc_inventory = os.path.join(rpc_repo_path,
-                             'rpc_deployment/inventory/dynamic_inventory.py')
+                             'playbooks/inventory/dynamic_inventory.py')
 subprocess.call([rpc_inventory] + sys.argv[1:])
