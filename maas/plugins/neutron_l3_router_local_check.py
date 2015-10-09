fc29b97d127fe15e9640269f9679889018b54902
Melvin Hillsman <codebauss@users.noreply.github.com>
2015-10-08 23:17:50 -0500

    Update neutron_l3_router_local_check.py

--- maas/plugins/neutron_l3_router_local_check.py
+++ maas/plugins/neutron_l3_router_local_check.py
@@ -12,29 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import argparse
-
-from maas_common import (status_ok, status_err, metric, metric_bool,
-                         get_neutron_client, get_auth_ref, get_endpoint_url_for_service, print_output)
+from maas_common import (status_ok,
+                         status_err,
+                         metric,
+                         metric_bool,
+                         get_auth_ref,
+                         get_endpoint_url_for_service,
+                         print_output)
 from requests import Session
 from requests import exceptions as exc
 
 def check(auth_ref):
     auth_token = auth_ref['token']['id']
-    
     # Use internalURL as this is a local plugin
     endpoint = get_endpoint_url_for_service('network', auth_ref['serviceCatalog'], url_type='internalURL')
-    
     # TODO set this from args.version to future-proof // version = args.version
     version = 'v2.0'
-    
     api_endpoint = '{endpoint}/{version}'.format(endpoint=endpoint,version=version)
-    
-    # Use get_neutron_client as it can check for stale token and get new one if necessary
-    neutron = get_neutron_client(endpoint_url=endpoint)
-
     s = Session()
-
     s.headers.update(
         {'Content-type': 'application/json',
          'X-Auth-Token': auth_token})
@@ -57,7 +52,6 @@ def check(auth_ref):
         else:
             routers = r.json()['routers']
 
-
     status_ok()
     metric_bool('neutron_api_status', is_active)
 
@@ -72,10 +66,10 @@ def check(auth_ref):
     # If router_status is ACTIVE, perform ping check
         if(router_status == 'ACTIVE'):
             import os, sys, time
-        
+
             # IP address is the WAN interface of the router
             ip_address = router['external_gateway_info']['external_fixed_ips'][0]['ip_address']
-        
+
             rc = os.system('ping -c1 -W3 ' + ip_address + ' > /dev/null')
             if(rc == 0):
 		pass
@@ -83,12 +77,11 @@ def check(auth_ref):
                 failed.append(router_name)
 
         failed_routers = ', '.join(failed)
-        
+
         if failed:
             return metric('neutron_router_ping', 'string', 'PING FAILURE: ' + failed_routers)
-        
-        return metric('neutron_router_ping', 'string', 'SUCCESS')
 
+        return metric('neutron_router_ping', 'string', 'SUCCESS')
 
 def main():
     auth_ref = get_auth_ref()
