d245a905a3c479944b4feb4ced26c41af0ffad91
Frank-ZhangXin <zxuuzx@gmail.com>
2017-10-16 17:19:56 -0500

    RLM-54 Fix ES upgrade log in leapfrog
    Elasicsearch log data would be lost during leapfrog process due to
    destroying Elasticsearch container. Solution is to preserve ES container
    from rebuild list and perform necessary upgrade playbook in post leap
    stage.

---
+++ scripts/leapfrog/elasticsearch_upgrade.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Copyright 2017, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## Shell Opts ----------------------------------------------------------------
+set -e -u -x
+set -o pipefail
+
+# Check upgrade flag
+if [[ -n "$UPGRADE_ELASTICSEARCH" ]]; then
+  if [[ "$UPGRADE_ELASTICSEARCH" == "no" ]]; then
+    echo 'Please make sure you want to upgrade Elasticsearch.'
+    exit
+  fi
+else
+  echo "Please preset env var before leap."
+  exit
+fi
+
+pushd /opt/rpc-openstack/rpcd/playbooks
+  # Update pip.conf on Elasticsearch container
+  openstack-ansible ../../scripts/leapfrog/playbooks/elasticsearch-postleap-pip-upgrade.yml
+
+  # Stop logstash service on Logstash container
+  openstack-ansible ../../scripts/leapfrog/playbooks/logstash-stop.yml
+
+  # Run elasticsearch upgrade playbook
+  openstack-ansible -e 'logging_upgrade=true' --tags 'reindex-wrapper,elasticsearch-upgrade' elasticsearch.yml
+
+  # Restore logstash service on Logstash container
+  openstack-ansible ../../scripts/leapfrog/playbooks/logstash-start.yml
+popd
