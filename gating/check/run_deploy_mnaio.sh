51baa7c4e46462b3a60e41963d5822c057f633fc
Dave Wilde <david.wilde@rackspace.com>
2018-09-19 10:41:29 -0500

    [RI-263] Deploy ELK in MNAIO
    This enables the ELK stack along with OS Profiler and UWSGI stats in the
    RPC-O MNAIO gate jobs.
    
    Issue: RI-263

--- gating/check/run_deploy_mnaio.sh
+++ gating/check/run_deploy_mnaio.sh
@@ -57,6 +57,42 @@ else
   sleep 300
   exit 0
 fi
+## OSA MNAIO Vars
+export PARTITION_HOST="true"
+export NETWORK_BASE="172.29"
+export DNS_NAMESERVER="8.8.8.8"
+export OVERRIDE_SOURCES="true"
+export DEVICE_NAME="vda"
+export DEFAULT_NETWORK="eth0"
+export DEFAULT_IMAGE="ubuntu-16.04-amd64"
+export DEFAULT_KERNEL="linux-image-generic"
+export SETUP_HOST="true"
+export SETUP_VIRSH_NET="true"
+export VM_IMAGE_CREATE="true"
+export DEPLOY_OSA="true"
+export OSA_BRANCH="${OSA_RELEASE}"
+export PRE_CONFIG_OSA="true"
+export RUN_OSA="false"
+export CONFIGURE_OPENSTACK="false"
+export DATA_DISK_DEVICE="sdb"
+export CONFIG_PREROUTING="false"
+export OSA_PORTS="6080 6082 443 80 8443"
+export RPC_BRANCH="${RE_JOB_BRANCH}"
+export DEFAULT_MIRROR_HOSTNAME=mirror.rackspace.com
+export DEFAULT_MIRROR_DIR=/ubuntu
+export INFRA_VM_SERVER_RAM=16384
+export MNAIO_ANSIBLE_PARAMETERS="-e default_vm_disk_mode=file"
+export DEPLOY_MAAS="false"
+export GATE_ELK="true"
+export DEPLOY_ELK=$(GATE_ELK:-true)
+export RUN_ELK=$(GATE_ELK:-true)
+# ssh command used to execute tests on infra1
+export MNAIO_SSH="ssh -ttt -oStrictHostKeyChecking=no root@infra1"
+# place variable in file to be sourced by parent calling script 'run'
+export MNAIO_VAR_FILE="${MNAIO_VAR_FILE:-/tmp/mnaio_vars}"
+echo "export MNAIO_SSH=\"${MNAIO_SSH}\"" > "${MNAIO_VAR_FILE}"
+echo "export RPC_RELEASE=\"${RPC_RELEASE}\"" >> "${MNAIO_VAR_FILE}"
+echo "export RPC_PRODUCT_RELEASE=\"${RPC_PRODUCT_RELEASE}\"" >> "${MNAIO_VAR_FILE}"
 
 ## Main --------------------------------------------------------------------
 
@@ -67,6 +103,40 @@ env | grep RE_ | while read -r match; do
   echo "export ${varName}='${!varName}'" >> /opt/rpc-openstack/RE_ENV
 done
 
+# checkout openstack-ansible-ops
+if [ ! -d "/opt/openstack-ansible-ops" ]; then
+  git clone --recursive https://github.com/openstack/openstack-ansible-ops /opt/openstack-ansible-ops
+else
+  pushd /opt/openstack-ansible-ops
+    git fetch --all
+  popd
+fi
+
+# apply various modifications for mnaio
+pushd /opt/openstack-ansible-ops/multi-node-aio
+  # By default the MNAIO deploys metering services, so we override
+  # osa_enable_meter to prevent those services from being deployed.
+  sed -i 's/osa_enable_meter: true/osa_enable_meter: false/' playbooks/group_vars/all.yml
+  # RI-263 Deploy ELK in the MNAIO gates
+  # Enables ELK, OS Profiler and UWSGI Stats
+  if [[ $GATE_ELK == "true" ]]; then
+    sed -i 's/osa_enable_elk_metrics: false/osa_enable_elk_metrics: true/' playbooks/group_vars/all.yml
+    sed -i 's/osa_enable_os_profiler: false/osa_enable_os_profiler: true/' playbooks/group_vars/all.yml
+    sed -i 's/osa_enable_uwsgi_stats: false/osa_enable_uwsgi_stats: true/' playbooks/group_vars/all.yml
+  fi
+popd
+
+# build the multi node aio
+pushd /opt/openstack-ansible-ops/multi-node-aio
+  # normally we can run ./build.sh by itself but gating environment requires
+  # this hack for now, have to set up env before updating cryptography
+  # run bootstrap first to set environment up
+  ./bootstrap.sh
+  # bump up cryptography version to avoid exception detailed in RLM-1104
+  pip install cryptography==1.5 --upgrade
+  # build the mnaio normally
+  ./build.sh
+popd
 echo "Multi Node AIO setup completed..."
 
 # capture all RE_ variables for push to infra1
